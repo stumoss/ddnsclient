1dff58407266606457ba1bad0a6f62cc646b416f
Stuart Moss <samoss@gmail.com>
2015-07-05 11:10:48 +0100

    Updated to use the authentication token correctly as it is just an alternative to your password

--- src/zoneedit/mod.rs
+++ src/zoneedit/mod.rs
@@ -2,34 +2,24 @@ extern crate regex;
 extern crate hyper;
 
 use self::hyper::Client;
-use self::hyper::header::{Headers, Authorization, Basic, Bearer};
+use self::hyper::header::{Headers, Authorization, Basic};
 use self::regex::Regex;
 use std::io::Read;
 
 static UPDATE_URL: &'static str = "https://dynamic.zoneedit.com/auth/dynamic.html";
 
 // Update the DNS entry
-pub fn dns_update(domain: &str, auth_token: &str,  username : &str, password : &str) -> Result<String, String> {
+pub fn dns_update(domain: &str, username : &str, password : &str) -> Result<String, String> {
     let mut headers = Headers::new();
 
-    if auth_token == "" {
-        headers.set(
-            Authorization(
-                Basic {
-                    username: username.to_string(),
-                    password: Some(password.to_string())
-                }
-                )
-            );
-    } else {
-        headers.set(
-            Authorization(
-                Bearer {
-                    token: auth_token.to_owned(),
-                }
-                )
-            );
-    }
+    headers.set(
+        Authorization(
+            Basic {
+                username: username.to_string(),
+                password: Some(password.to_string())
+            }
+            )
+        );
 
     let full_www_url = format!("{}?zones={}", UPDATE_URL, domain);
     let client = Client::new();
@@ -46,7 +36,10 @@ pub fn dns_update(domain: &str, auth_token: &str,  username : &str, password : &
 // Parse the return code
 fn parse_return_code(response: &str) -> Result<String, String> {
     let re = Regex::new("<\\w.*=\"(\\d+)\"\\s+TEXT=\"(\\w[^\"]*)\"\\s+ZONE=\"([^\"]*)\">").unwrap();
-    let cap = re.captures(&response).unwrap();
+    let cap = match re.captures(&response) {
+        Some(cap) => cap,
+        None => return Err(format!("{}", response)),
+    };
 
     let return_code = cap.at(1).unwrap().parse();
     match return_code {
